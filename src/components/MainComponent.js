c5b3e9e5fc81d26c36b33b88415b6d271c811c38
tran-hong-chi-khang <khangdvooo@gmail.com>
2020-09-07 22:06:07 +0700

    Controlled Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,8 +5,8 @@ import Header from './HeaderComponent';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
-
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -56,6 +56,7 @@ class Main extends Component {
 					<Route path='/home' component={HomePage} />
 					<Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
 					<Route exact path='/contactus' component={Contact} />
+					<Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
 					<Route path='/menu/:dishId' component={DishWithId} />
 					<Redirect to='/home' />
 				</Switch>
