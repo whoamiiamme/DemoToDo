39f34c171969f9816c2b77bf1020d844e7d1da9b
tran-hong-chi-khang <khangdvooo@gmail.com>
2020-09-27 20:43:32 +0700

    Fetch Handling Errors

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Footer from './FooterComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 const mapStateToProps = (state) => {
@@ -27,6 +27,12 @@ const mapDispatchToProps = (dispatch) => ({
 	},
 	resetFeedbackForm: () => {
 		dispatch(actions.reset('feedback'));
+	},
+	fetchComments: () => {
+		dispatch(fetchComments());
+	},
+	fetchPromos: () => {
+		dispatch(fetchPromos());
 	}
 });
 
@@ -37,6 +43,8 @@ class Main extends Component {
 
 	componentDidMount() {
 		this.props.fetchDishes();
+		this.props.fetchComments();
+		this.props.fetchPromos();
 	}
 
 	render() {
@@ -46,7 +54,9 @@ class Main extends Component {
 					dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
 					dishesLoading={this.props.dishes.isLoading}
 					dishesErrMess={this.props.dishes.errMess}
-					promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+					promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+					promoLoading={this.props.promotions.isLoading}
+					promoErrMess={this.props.promotions.errMess}
 					leader={this.props.leaders.filter((leader) => leader.featured)[0]}
 				/>
 			);
@@ -61,6 +71,7 @@ class Main extends Component {
 					comments={this.props.comments.filter(
 						(comment) => comment.dishId === parseInt(match.params.dishId, 10)
 					)}
+					commentsErrMess={this.props.comments.errMess}
 					addComment={this.props.addComment}
 				/>
 			);
